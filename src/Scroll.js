983a143b716d9f14e4c1232313d13a1ff73e8742
nargilabar <45494495+nargilabar@users.noreply.github.com>
2021-03-18 19:29:01 +0100

    adding scrolling on cards

---
+++ src/Scroll.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Scroll = (props) => {
+	return (
+		<div style = {{ overflowY: 'scroll', border: '5px solid black', height: '650px' }}>  
+			{props.children}
+		</div>
+	);
+}
+
+export default Scroll;
+//overflow-y in css
+//overflowY in jsx
